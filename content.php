771b84c51bba5736f5c6127ac23cdc2890bd7192
Ronny Perez <ronnypdev@gmail.com>
2018-03-27 22:41:56 -0400

    content file documented

--- content.php
+++ content.php
@@ -5,7 +5,7 @@ path to cache JSON File
 $cache_path = 'cache/cache.json';
 $api_url = "http://rentcafe.com/rentcafeapi.aspx?requestType=apartmentavailability&APIToken=NDY5OTI%3d-XDY6KCjhwhg%3d&propertyCode=p0155985";
 
-// Cache file not older than 10 mins. 
+// Cache file not older than 10 mins.
 if (file_exists($cache_path) && (filemtime($cache_path) > (time() - 60 * 10 ))) {
    
    $data = json_decode(file_get_contents($api_url), true);
